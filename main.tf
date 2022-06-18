57eed4ff97bf87bd1960d486134af2604bb9719d
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-06-18 18:33:49 +0300

    chore: file naming convention

--- main.tf
+++ main.tf
@@ -13,20 +13,20 @@ locals {
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "~> 3.14"
 
   create_vpc = true
-  name = var.vpc_name
-  cidr = var.cidr
+  name       = var.vpc_name
+  cidr       = var.cidr
 
   azs             = data.aws_availability_zones.available.names
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
-  one_nat_gateway_per_az = false
+  enable_nat_gateway           = true
+  single_nat_gateway           = true
+  one_nat_gateway_per_az       = false
   create_database_subnet_group = false
 
   enable_vpn_gateway = false
